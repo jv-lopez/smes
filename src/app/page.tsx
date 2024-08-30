9ec0f4cd1e86f1f9a28c1626cacc6457a0476cbf
jv-lopez <93750956+jv-lopez@users.noreply.github.com>
2024-08-30 08:25:16 +0800

    feat: create workspace modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 'use client';
+import { useEffect, useMemo } from 'react';
 
 import { UserButton } from '@/features/auth/components/user-button';
-import { useAuthActions } from '@convex-dev/auth/react';
+import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces';
+import { useCreateWorkspaceModal } from '@/features/workspaces/store/use-create-workspace-modal';
 
 export default function Home() {
-  const { signOut } = useAuthActions();
+  const [open, setOpen] = useCreateWorkspaceModal();
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (workspaceId) {
+      console.log('Redirect to workspace');
+    } else if (!open) {
+      console.log('Open creation modal');
+      setOpen(true);
+    }
+  }, [workspaceId, isLoading, open, setOpen]);
+
   return (
     <div>
-      logged in
       <UserButton />
     </div>
   );
---
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { useCreateWorkspaceModal } from '@/features/workspaces/store/use-create-workspace-modal';
+
+export const CreateWorkspaceModal = () => {
+  const [open, setOpen] = useCreateWorkspaceModal();
+
+  const handleClose = () => {
+    setOpen(false);
+    //TODO: Clear form
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a workspace</DialogTitle>
+        </DialogHeader>
+        <form className="space-y-4">
+          <Input
+            value={''}
+            disabled={false}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={false}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
