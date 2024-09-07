1b9737c817ca97898ab17706ce48d6926f150e8b
jv-lopez <93750956+jv-lopez@users.noreply.github.com>
2024-09-07 09:55:35 +0800

    feat: workspace sidebar, sidebar header and settings dropdown

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,6 +1,14 @@
 'use client';
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
+
 import { Sidebar } from '@/app/workspace/[workspaceId]/sidebar';
 import { Toolbar } from '@/app/workspace/[workspaceId]/toolbar';
+import { WorkspaceSidebar } from '@/app/workspace/[workspaceId]/worspace-sidebar';
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
@@ -12,7 +20,20 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="smes-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
